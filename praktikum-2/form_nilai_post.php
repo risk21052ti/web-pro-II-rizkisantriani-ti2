c179adee5ea3018c2fd4d55103dd1303c3d012fc
rizki <risk21052ti@student.nurulfikri.ac.id>
2022-03-09 20:01:15 +0700

    add praktum-2

---
+++ praktikum-2/form_nilai_post.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Nilai POST</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container-card">
+        <div class="row">
+            <div class="col-12 bg-light pt-3">
+                <p>Sistem Penilaian</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p>Form Nilai Siswa</p>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form method="POST">
+                    <div class="form-group row">
+                        <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+                        <div class="col-8">
+                        <input id="nama" name="nama" placeholder="Masukan Nama Lengkap" type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+                        <div class="col-8">
+                        <input id="matkul" name="matkul" placeholder="Masukan Mata Kuliah " type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label> 
+                        <div class="col-8">
+                        <input id="nilai_uts" name="nilai_uts" placeholder="Masukan Nilai UTS" type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nilai_uas" class="col-4 col-form-label">Nilai UAS</label> 
+                        <div class="col-8">
+                        <input id="nilai_uas" name="nilai_uas" placeholder="Masukan Nilai UAS" type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nilai_tugas" class="col-4 col-form-label">Nilai Tugas</label> 
+                        <div class="col-8">
+                        <input id="nilai_tugas" name="nilai_tugas" placeholder="Masukan Nilai Tugas" type="text" class="form-control">
+                        </div>
+                    </div> 
+                    <div class="form-group row">
+                        <div class="offset-4 col-8">
+                        <button name="proses" type="submit" class="btn btn-primary">Kirim</button>
+                        </div>
+                    </div>
+                </form>
+                <?php
+                    $proses = $_POST['proses'];
+                    $nama = $_POST['nama'];
+                    $mata_kuliah = $_POST['matkul'];
+                    $nilai_uts = $_POST['nilai_uts'];
+                    $nilai_uas = $_POST['nilai_uas'];
+                    $nilai_tugas = $_POST['nilai_tugas'];
+
+                    echo "Nama : $nama";
+                    echo "<br/>Mata Kuiah : $mata_kuliah";
+                    echo "<br/>Nilai UTS : $niali_uts";
+                    echo "<br/>Nilai UAS : $nilai_uas";
+                    echo "<br/>Nilai Tugas : $nilai_tugas";
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 bg-lights pt-3">
+                <p>Develope by Mahasiswa STT NF</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
